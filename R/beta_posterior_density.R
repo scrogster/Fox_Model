a7531b99bdd89defd575ceee4e1910c6f6b63747
michael scroggie <scrogster@gmail.com>
2016-05-27 16:57:12 +1000

    attempt to get greek letters in strip headings. Not working yet.

--- R/beta_posterior_density.R
+++ R/beta_posterior_density.R
@@ -1,10 +1,13 @@
 ## ----mcmc_plot_beta, echo=FALSE, cache=FALSE, fig.cap='Posterior distributions of the regression parameters ($\\beta$).', dependson="hier_model", fig.pos="p!", message=FALSE----
 require(ggplot2)
 require(ggmcmc)
+require(grid)
 
 load("model_results.Rdata")
 
 beta_result<-ggs(samp, family="^beta|^r.mean")
+
+
 ggplot(beta_result, aes(x=value, group=Parameter))+
 	geom_density(fill="red", alpha=0.3)+
 	xlab("")+
@@ -12,6 +15,15 @@ ggplot(beta_result, aes(x=value, group=Parameter))+
 	facet_wrap(~Parameter)+
 	geom_vline(xintercept=0, col="black", linetype = "longdash")+
 	theme_classic()
+
+#http://stackoverflow.com/questions/11979017/changing-facet-label-to-math-formula-in-ggplot2
+for(ii in 1:7){
+	grid.gedit(gPath(paste0("strip_t-", ii), "strip.text"), 
+			 grep=TRUE, label=bquote(beta[.(ii)]))}
+grid.gedit(gPath(paste0("strip_t-", 8), "strip.text"), 
+		 grep=TRUE, label=bquote(bar(r)[fox]))
+grid.gedit(gPath(paste0("strip_t-", 9), "strip.text"), 
+		 grep=TRUE, label=bquote(bar(r)[rabbit]))
 
 ggsave("Figures/beta_posterior_density.pdf")
 ggsave("Figures/beta_posterior_density.png")
--- R/sigma_posterior_density.R
+++ R/sigma_posterior_density.R
@@ -1,30 +1,36 @@
 require(ggplot2)
 require(ggmcmc)
 require(dplyr)
+library(grid)
 
 load("model_results.Rdata")
 
 beta_result<-ggs(samp, family="^sigma")
 
-signames <- c(
-	'sigma[1]'= "sigma[proc] (fox)",         #"\\sigma[proc] (fox)",
-	'sigma[2]'="sigma[proc] (rabbit)",
-	'sigma[3]'="sigma[transect] (fox)",
-	'sigma[4]'="sigma[transect] (rabbit)"
-)
+# signames <- c(
+# 	'sigma[1]'= "sigma[proc] (fox)",         #"\\sigma[proc] (fox)",
+# 	'sigma[2]'="sigma[proc] (rabbit)",
+# 	'sigma[3]'="sigma[transect] (fox)",
+# 	'sigma[4]'="sigma[transect] (rabbit)"
+# )
 
-#faclab<-function(variable, value){
-#  return(signames[value])
-#  llply(as.character(signames[value]), function(x) parse(text = x))
-#}
 
 ggplot(beta_result, aes(x=value, group=Parameter))+
 	geom_density(fill="red", alpha=0.3)+
-	facet_grid(.~Parameter)+ #labeller = faclab)+
+	facet_wrap(~Parameter)+
 	xlab("")+
 	ylab("")+
 	theme_classic()+
 	theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
+grid.gedit(gPath(paste0("strip_t-", 1), "strip.text"), 
+		 grep=TRUE, label=bquote(sigma[proc](fox)))
+grid.gedit(gPath(paste0("strip_t-", 2), "strip.text"), 
+		 grep=TRUE, label=bquote(sigma[proc](rabbit)))
+grid.gedit(gPath(paste0("strip_t-", 3), "strip.text"), 
+		 grep=TRUE, label=bquote(sigma[transect](fox)))
+grid.gedit(gPath(paste0("strip_t-", 4), "strip.text"), 
+		 grep=TRUE, label=bquote(sigma[transect](rabbit)))
 
 ggsave("Figures/sigma_posterior_density.pdf")
 ggsave("Figures/sigma_posterior_density.png")
