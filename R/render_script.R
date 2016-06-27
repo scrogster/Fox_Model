cd90c59b8df6b3020aaa58fd3e915d7726b31d31
michael scroggie <scrogster@gmail.com>
2016-06-27 17:54:05 +1000

    deleted arguments for now, will fix this feature once lag models are up and running and choice is clear.

--- R/render_script.R
+++ R/render_script.R
@@ -2,9 +2,6 @@ require(rmarkdown)
 
 args=commandArgs(trailingOnly=TRUE)
 
-paper=file.path(args[1])
-supp=file.path(args[2])
-
 rmarkdown::render("Fox_model_paper.Rmd")
 
 rmarkdown::render("Supporting_Information.Rmd")
