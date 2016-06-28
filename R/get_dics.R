aa82d29534849c846f197d9977d869fa887fad24
michael scroggie <scrogster@gmail.com>
2016-06-28 15:12:12 +1000

    had accidently duplicated model 0_6, and omitted model 6_6. Fixed now.

--- R/get_dics.R
+++ R/get_dics.R
@@ -7,7 +7,7 @@ result_list<-list(
 	"model_results_0_0.Rdata",
 	"model_results_0_6.Rdata",
 	"model_results_6_0.Rdata",
-	"model_results_0_6.Rdata",
+	"model_results_6_6.Rdata",
 	"model_results_6_12.Rdata",
 	"model_results_12_6.Rdata",
 	"model_results_0_12.Rdata",
