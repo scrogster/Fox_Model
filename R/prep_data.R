0ce7d9b96e7a320f7d66c22cc8fc7e5d07a49b86
michael scroggie <scrogster@gmail.com>
2017-08-22 09:02:41 +1000

    updated data preparation to allow for distributed lags

--- R/prep_data.R
+++ R/prep_data.R
@@ -5,66 +5,168 @@ library(readr)
 library(tidyr)
 library(stringr)
 
+#read and tidy the rain data
+allrain<-read_csv("Data/AllRain.csv") %>%
+	select(-X1 , -Spring) %>%
+	filter(Site != "Avalon" & Site != "Sunbury")
+
+allrain_pentland<-allrain %>%
+	filter(Site=="Ingliston") %>%
+	mutate(Site=replace(Site, Site=="Ingliston", "Pentland Hills"))
+allrain<-rbind(allrain, allrain_pentland)
+rm(allrain_pentland)
+allrain<-distinct(allrain) %>% #drop duplicate rows!!!
+	mutate(Deemed_date=ymd(paste(Year, Month, 15))) %>%
+     mutate(Site=replace(Site, Site=="BlackRange", "Black Range")) %>%
+     mutate(Site=replace(Site, Site=="SwiftsCreek", "Swifts Creek")) %>%
+     mutate(Site=replace(Site, Site=="Yarram/Woodside", "Yarram")) 
 
 
-## Import and tidy the rainfall data for the sites, including imputing missing data for Pentland Hills site
-rain_summary<-read_csv("Data/TabulatedRainHalfYearly_updated.csv") %>%
-	rename(Site=X1) %>%
-	filter(Site != "Avalon" & Site != "Sunbury") 
-#add in rainfall for Pentland Hills (using data from nearby Ingliston Site)
-pentland_rain<-rain_summary[rain_summary$Site=="Ingliston",]
-pentland_rain$Site<-"Pentland Hills"
-new_rain<-rbind(rain_summary[1:12,], pentland_rain, rain_summary[13:20,])
-
-rain_summary<-new_rain
-rm(new_rain)
-rm(pentland_rain)
-
-# tidy the raindat
-tidy_rain<-rain_summary %>%
-	gather(timestep, rain, 2:43) %>%
-	arrange(Site) %>%
-	separate(timestep, into=c("Year", "Step"), sep="--") %>%
-	mutate(ytime_disc=as.numeric(Year) + as.numeric(Step)*0.5)
+#read and tidy the SOI data. Note months are as three letter strings.
+SOI<-read_table("data/SOI.txt") %>%
+	gather("month", "SOI", 2:13) %>%
+	mutate(Deemed_date=ymd(paste(Year, month, 15)))  #just puts the assumed date midmonth
 
 
-#this block of code adds a variable to denote each unique session for computing averages etc.
+
 func<-function(x){
 	incr=0
 	out<-numeric(length(x))
 	for(i in 1: length(x)){
 		if(x[i]==1) {incr=incr+1}
-		out[i]=incr 
+		out[i]=incr
 	}
 	return(out)
 }
-##Import and tidy the spotlight data.
+#tidying and discretizing the spotlight data
 spotlight<-read_csv("Data/spotlight_data.csv") %>%
 	rename(Site=`Monitor.Site`, Date=`Spotlight.Date`) %>%
 	filter(Site != "Pyramid Hill") %>%
 	arrange(Site, Date)  %>%
 	mutate(first=(Replicate_Count==1)) %>%
-	mutate(session= func(first))
-
-
-#calculate a mid-point date for each session of replicate counts and append to the dataframe
-spotlight<-spotlight %>%
+	mutate(session= func(first)) %>%
 	group_by(session) %>%
-	mutate(SessDate=mean(Date)) %>%
-	mutate(ytime=decimal_date(SessDate) ) %>%
-	mutate(ytime_disc=floor(ytime/0.5)*0.5) %>%  #this discretizes the dates.
-	mutate(time_idx=1+(ytime_disc-1998)*2  ) %>% #this is a six-monthly time index for use in the models starts at first half of 1998
+	mutate(SessDate=mean(Date)) %>%   #within a session the deemed date of the survey is the mean date
+	ungroup() %>%
+	mutate(Semester=semester(SessDate), Year=year(SessDate)) %>%   #which year and semester were each session in
+	mutate(ytime_disc = Year + (Semester-1)/2) %>%    #making times discrete
+	mutate(time_idx = 5+(ytime_disc-1998)*2) %>%     #integer flags for successive semesters  #first surveys at time 3
 	mutate(site_code=as.numeric(factor(Site))) %>% #these are numeric site indices
-	mutate(winter =  (ytime_disc %% 1) *2 )    #these are flags for second half of year (implies post-winter census)
+	mutate(winter =  (ytime_disc %% 1) *2 )  %>%
+	mutate(Site=replace(Site, Site=="Yarram/Woodside", "Yarram"))   #these are flags for second half of year (implies post-winter census)
 
 #need to add in a winter variable to code for counts made after the winter interval (second half of year)                                                
 
-start_times = spotlight %>% group_by(Site) %>% summarise(start_times=min(time_idx))
+start_times = spotlight %>% group_by(Site) %>% summarise(start_times=min(time_idx), start_dates=min(SessDate))
 
-end_times = spotlight %>% group_by(Site) %>% summarise(end_times=max(time_idx))
+end_times = spotlight %>% group_by(Site) %>% summarise(end_times=max(time_idx), end_dates=max(SessDate))
 
-rain_mat<-as.matrix( (rain_summary[,-1]-250)/100)  #rain data roughly centred by subtracting 250, and scaled by dividing by 100. 
+#key for matching time_idx with Year/Semester designation...
+time.key<-data.frame("time_idx"=seq(1:42), "Year"=rep(1996:2016, each=2), "Semester"=c(1:2))
 
+###############################################################################
+#   MAKING LAGGED RAINFALL ARRAYS..
+###############################################################################
+
+preds<-expand.grid(time_idx=1:42,  Site=levels(factor(spotlight$Site)), lag=1:30) %>%
+	left_join(time.key, by="time_idx") %>% 
+	select(time_idx, Site, Year, Semester, lag) %>%
+	mutate(Site=as.character(Site))
+
+#This function grabs months rainfall by site, year, semester and lag.
+get_rain<-function(Sitename="Ararat", Year=1998, Sem=1, lag=0){
+	if(Sem==1){month=4} else {month=11} #we'll assume April and November surveys
+	DD<-ymd(paste(Year, month, 15)) %m-% months(as.numeric(lag))
+	outrain<-allrain %>%
+		filter(Site==Sitename, Deemed_date==DD) %>%
+		head(1)
+	outval<-outrain
+	if(length(outval$Rain==1))  {return(outval$Rain)} else {return(NA)}
+}
+
+get_rain2<-function(x){
+	out<-get_rain(Sitename = x[,"Site"], Year=x[,"Year"], Sem=x[,"Semester"], lag=x[,"lag"])
+	return(out)
+}
+
+rainpred<-numeric(nrow(preds))
+for(i in 1:nrow(preds)){
+rainpred[i]<-get_rain2(preds[i,])
+}; rm(i)
+
+#append rain vector to pred dataframe
+preds<-data.frame(preds, "rain"=rainpred)
+
+#this makes the array with dimensions (Site, time_idx and lag)
+rain_lag_array<-tapply(preds$rain, preds[,c(2, 1,5)], min, na.rm=FALSE)
+
+rain_array2<-rain_lag_array  #this will be a version with cumulative means over increasing lags
+
+dim(rain_array2)
+#sweep out cumulative means along increasing lags...
+for(i in 1:21){  #sites
+	for(j in 1:42){  #times
+	 for(k in 1:30){ #lags
+	 	rain_array2[i,j,k]<-mean(rain_lag_array[i,j,1:k], na.rm=TRUE)	
+	 }	
+	}
+}; rm(i); rm(j);  rm(k)
+
+###############################################################################
+#   MAKING LAGGED SOI ARRAY.
+############################################################################### 
+
+preds_soi<-expand.grid(time_idx=1:42 , lag=1:30) %>%
+	left_join(time.key, by="time_idx") %>% 
+	select(time_idx,  Year, Semester, lag) 
+
+
+get_soi<-function(Year=1998, Sem=1, lag=0){
+	if(Sem==1){month=4} else {month=11} #we'll assume April and November surveys
+	DD<-ymd(paste(Year, month, 15)) %m-% months(as.numeric(lag))
+	outsoi<-SOI %>%
+		filter(Deemed_date==DD) %>%
+		head(1)
+	outval<-outsoi
+	if(length(outsoi$SOI==1))  {return(outval$SOI)} else {return(NA)}
+}
+
+get_soi2<-function(x){
+	out<-get_soi( Year=x[,"Year"], Sem=x[,"Semester"], lag=x[,"lag"])
+	return(out)
+}
+
+
+soipred<-numeric(nrow(preds_soi))
+for(i in 1:nrow(preds_soi)){
+	soipred[i]<-get_soi2(preds_soi[i,])
+}; rm(i)
+
+#append rain vector to pred dataframe
+preds_soi<-data.frame(preds_soi, "soi"=soipred)
+
+soi_lag_array<-tapply(preds_soi$soi, preds_soi[,c(1, 4)], min, na.rm=FALSE)
+
+soi_array2<-soi_lag_array  #this will be a version with cumulative means over increasing lags
+
+#sweep out cumulative means along increasing lags...
+
+	for(j in 1:42){  #times
+		for(k in 1:30){ #lags
+			soi_array2[j,k]<-mean(soi_lag_array[j,1:k], na.rm=TRUE)	
+		}	
+	}; rm(j);  rm(k)
+
+
+###############################################################################
+#   INSERT CODE HERE FOR LAGGED NDVI ARRAYS
+###############################################################################
+
+
+
+###############################################################################
+#   MAKNG DATA OBJECT (LIST) FOR MODELLING IN JAGS
+###############################################################################
 #tidy this up with new variables, and all good.
 hier_dat<-list(
 	site.code=as.numeric(factor(spotlight$Site)),
@@ -72,14 +174,14 @@ hier_dat<-list(
 	rabbit.count=spotlight$Rabbits,
 	trans.length=spotlight$TransectLength,
 	obs_time=spotlight$time_idx,
-	start=start_times$start_times,
-	end=  end_times$end_times,
+#	start=start_times$start_times,
+#	end=  end_times$end_times,
 	sites=max(as.numeric(factor(spotlight$Site))),
 	Nobs=nrow(spotlight),
 	winter=spotlight$winter,
-	postrip=spotlight$PostRipped,
-	rain= rain_mat, 
-	rain.offset=4   #value of 4= six-month lag, 3=1 year lag, #can smooth later.
+	postrip=as.numeric(spotlight$PostRipped),
+     rain_array = rain_array2,  #site by time by lag matrix
+     soi_array  = soi_array2   #time by lag matrix
 )
 
 save.image("prepped_data.Rdata")
