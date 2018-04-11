709ad82e71eccb611f631d24574794a611c9340b
michael scroggie <scrogster@gmail.com>
2018-04-11 17:45:33 +1000

    Extensive revision of entire text

--- R/beta_posterior_density.R
+++ R/beta_posterior_density.R
@@ -1,4 +1,3 @@
-## ----mcmc_plot_beta, echo=FALSE, cache=FALSE, fig.cap='Posterior distributions of the regression parameters ($\\beta$).', dependson="hier_model", fig.pos="p!", message=FALSE----
 require(ggplot2)
 require(ggmcmc)
 require(grid)
@@ -18,36 +17,36 @@ load(file.path(model_data))
 
 beta_result<-data.frame(cbind(samp$sims.list$beta
 						)) %>%
-	               rename('beta[1]'=X1, 
-	               	  'beta[2]'=X2, 
-	               	  'beta[3]'=X3, 
-	               	  'beta[4]'=X4,
-	               	  'beta[5]'=X5, 
-	               	  'beta[6]'=X6,
-	               	  'beta[7]'=X7, 
-	               	  'beta[8]'=X8
+	               rename('beta[5]'=X1, 
+	               	  'beta[6]'=X2, 
+	               	  'beta[7]'=X3, 
+	               	  'beta[8]'=X4,
+	               	  'beta[1]'=X5, 
+	               	  'beta[2]'=X6,
+	               	  'beta[3]'=X7, 
+	               	  'beta[4]'=X8
 	               	  ) %>%
 					gather()	%>%
 	                    rename(Parameter=key) %>%
 	   mutate()
 
-beta_result$species[grep("[1234]", beta_result$Parameter)]<-"Fox"
-beta_result$species[grep("[5678]", beta_result$Parameter)]<-"Rabbit"
-				
+beta_result$species[grep("[1234]", beta_result$Parameter)]<-"Rabbit"
+beta_result$species[grep("[5678]", beta_result$Parameter)]<-"Fox"
+
 SS<-beta_result %>%
 	group_by(Parameter) %>%
 	summarise(mean=mean(value), sd=sd(value), lwr=quantile(value, 0.025), upp=quantile(value, 0.975))
 
 beta_graph<-ggplot(beta_result, aes(x=Parameter, y=value, fill=species)) +
 	geom_violin(scale="width", alpha=0.6, draw_quantiles = c(0.025, 0.5, 0.975)) +
-	scale_x_discrete(labels=expression(atop(beta[1], NR(fox)), 
-								atop(beta[2], DD(fox)), 
-								atop(beta[3], rain(fox)), 
-								atop(beta[4], winter(fox)), 
-								atop(beta[5], DD(rabbit)), 
-								atop(beta[6], rain(rabbit)), 
-								atop(beta[7], winter(rabbit)), 
-								atop(beta[8], ripping(rabbit))))+
+	scale_x_discrete(labels=expression(atop(beta[1], DD(rabbit)), 
+								atop(beta[2], rain(rabbit)), 
+								atop(beta[3], winter(rabbit)), 
+								atop(beta[4], ripping(rabbit)), 
+								atop(beta[5], NR(fox)), 
+								atop(beta[6], DD(fox)), 
+								atop(beta[7], rain(fox)),
+								atop(beta[8], winter(fox))))+
 	scale_fill_manual(values=c("darkorange3", "blue"))+
 	geom_hline(yintercept=0, col="black", linetype = "longdash")+
 	xlab("")+
@@ -73,6 +72,8 @@ lag_result<-data.frame(cbind(samp$sims.list$fox.lag,
 
 lag_result$species[grep("fox", lag_result$Parameter)]<-"Fox"
 lag_result$species[grep("rabbit", lag_result$Parameter)]<-"Rabbit"
+
+lag_result$Parameter<-factor(lag_result$Parameter, levels=c("k[rabbit]", "k[fox]", "k[NR]"))
 
 lag_graph<-ggplot(lag_result, aes(x=value, fill=species)) +
 	geom_histogram(alpha=0.6, binwidth=1) +
