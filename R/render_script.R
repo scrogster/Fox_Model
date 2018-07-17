a48c9a41f8e9b717b4df7c1ad3704fb4668de859
scrogster <scrogster@gmail.com>
2018-07-17 14:31:11 +1000

    Supplementary files now placed in own directory

--- R/render_script.R
+++ R/render_script.R
@@ -9,14 +9,16 @@ out_format=args[1]
 #render the manuscript
 if(out_format=="pdf_document") {render("Fox_model_paper.Rmd", "pdf_document")}
 
+#Render the supporting information
+dir.create("SupportingInformation", showWarnings = FALSE)
 #render the supplementary tables
-if(out_format=="pdf_document") {render("Table_S1.Rmd", "pdf_document", "JAEscroggieST1.pdf")}
-if(out_format=="pdf_document") {render("Table_S2.Rmd", "pdf_document", "JAEscroggieST2.pdf")}
+if(out_format=="pdf_document") {render("Table_S1.Rmd", "pdf_document", "SupportingInformation/JAEscroggieST1.pdf")}
+if(out_format=="pdf_document") {render("Table_S2.Rmd", "pdf_document", "SupportingInformation/JAEscroggieST2.pdf")}
 
 #render the supplementary figures
-if(out_format=="pdf_document") {render("Figure_S1_withCaption.Rmd", "pdf_document", "JAEscroggieSF1.pdf")}
-if(out_format=="pdf_document") {render("Figure_S2_withCaption.Rmd", "pdf_document", "JAEscroggieSF2.pdf")}
+if(out_format=="pdf_document") {render("Figure_S1_withCaption.Rmd", "pdf_document", "SupportingInformation/JAEscroggieSF1.pdf")}
+if(out_format=="pdf_document") {render("Figure_S2_withCaption.Rmd", "pdf_document", "SupportingInformation/JAEscroggieSF2.pdf")}
 
 
 #render the appendix
-if(out_format=="pdf_document") {render("Appendix_S1.Rmd", "pdf_document", "JAEscroggieSA1.pdf")}
+if(out_format=="pdf_document") {render("Appendix_S1.Rmd", "pdf_document", "SupportingInformation/JAEscroggieSA1.pdf")}
